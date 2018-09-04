3567da526e67ad92c3245f733b5c08829c57cee5
jslucassf <jslucassf@gmail.com>
2018-09-03 22:36:12 -0300

    Refatoramento de código

--- 01 - Regressão Linear Simples/grad_desc.py
+++ 01 - Regressão Linear Simples/grad_desc.py
@@ -3,7 +3,6 @@ import math
 
 RSS_points = []
 gradient_size = []
-iterations = []
 
 verbose = True
 
@@ -18,7 +17,7 @@ def compute_error_for_given_points(b, m, points):
     
     return total_error / float(len(points))
 
-def step_gradient(b_current, m_current, points, learning_rate):
+def step_gradient(points, b_current, m_current, learning_rate):
     # Gradient Descent
     b_gradient = 0
     m_gradient = 0
@@ -43,55 +42,20 @@ def step_gradient(b_current, m_current, points, learning_rate):
 
     return [new_b, new_m, b_gradient, m_gradient]
 
-def compute_gradient_size(b, m):
-    return math.sqrt(b ** 2 + m ** 2)
+def gradient_desc_iter(points, b, m, learning_rate, num_iterations):
+    for i in range(num_iterations):
+        results = step_gradient(array(points), b, m, learning_rate)
+        b = results[0]
+        m = results[1]
 
-def step_gradient_norm(b_current, m_current, points, learning_rate, gradient_threshold):
-    # Gradient Descent
-    b_gradient = 0
-    m_gradient = 0
-    N = float(len(points))
+    return results[:2]
 
-    
-    while True:
-        for i in range(0, len(points)):
-            x = points[i, 0]
-            y = points[i, 1]
-
-            b_gradient += -(2/N) * (y - (b_current + (m_current * x)))
-            m_gradient += -(2/N) * ((y - (b_current + (m_current * x))) * x)
-
-        new_b = b_current - (learning_rate * b_gradient)
-        new_m = m_current - (learning_rate * m_gradient)
-
-        grad_sz = compute_gradient_size(b_gradient, m_gradient)
-        gradient_size.append(grad_sz)
-        print(linalg.norm([b_gradient, m_gradient]))
-        #print(new_b, new_m)
-        #print(b_gradient, m_gradient)
-        #print(grad_sz, gradient_threshold)
-        if(grad_sz < gradient_threshold):
-            break
-        
-    RSS = compute_error_for_given_points(new_b, new_m, points)
-    RSS_points.append(RSS)
-    
-    if(verbose):
-        print("For values of:\nb = {0}\nm = {1}\nThe RSS is {2}"
-            .format(new_b, new_m, RSS))
-
-    return [new_b, new_m]
-
-def gradient_desc_norm(points, starting_b, starting_m, learning_rate, gradient_threshold):
-    b = starting_b
-    m = starting_m
-    
+def gradient_desc_norm(points, b, m, learning_rate, gradient_threshold):
     gradient_norms = []
-        
     iteration = 1
         
     while True:
-        b, m, b_gradient, m_gradient = step_gradient(b, m, array(points), learning_rate)
+        b, m, b_gradient, m_gradient = step_gradient(array(points), b, m, learning_rate)
         gradient_norm = linalg.norm([b_gradient, m_gradient])
         
         gradient_norms.append(gradient_norm)
@@ -106,7 +70,7 @@ def gradient_desc_norm(points, starting_b, starting_m, learning_rate, gradient_t
 def normal_equations(points):
     mean_x, mean_y = points.mean(0)
     
-    # m
+    # m = ...
     num = 0
     den = 0
     for i in range(len(points)):
@@ -123,53 +87,29 @@ def normal_equations(points):
     
     return [new_b, new_m, 0, 0]
     
+def gradient_desc_runner(points, b, m, learning_rate, num_iterations, running_mode, gradient_threshold):
+    if(running_mode == 1):
+        print("Running Gradient Descent using the number of iterations as a stoping condition")
+        return gradient_desc_iter(array(points), b, m, learning_rate, num_iterations)
+    elif(running_mode == 2):
+        print("Running Gradient Descent using the norm's size as a stoping condition")
+        return gradient_desc_norm(array(points), b, m, learning_rate, gradient_threshold)
+    elif(running_mode == 3):
+        print("Running Gradient Descent using the normal equations")
+        return normal_equations(array(points))
 
-def gradient_desc_runner(points, starting_b, starting_m, learning_rate, num_iterations, stopping_criteria, gradient_threshold):
-    b = starting_b
-    m = starting_m
-    
-    i = 0
-
-    if(stopping_criteria == 1):
-        while True:
-            b, m = step_gradient(b, m, array(points), learning_rate)
-
-            grad_sz = compute_gradient_size(b, m)
-            gradient_size.append(grad_sz)
-            iterations.append(i)
-
-            if(verbose):
-                print("Gradient size: {0}".format(grad_sz))
-
-            if(stopping_criteria == 1):
-                if(i >= num_iterations):
-                    break
-            elif(stopping_criteria == 2):
-                if(grad_sz >= gradient_threshold):
-                    break
-
-            i += 1
-    elif(stopping_criteria == 2):
-        results = gradient_desc_norm(array(points), b, m, learning_rate, gradient_threshold)
-    elif(stopping_criteria == 3):
-        results = normal_equations(array(points))
-
-    return results
-
-
-
-
-# Stopping Criteria takes one of two values
+# Running Mode takes one of two values
 #   1 - Number of iterations
 #   2 - Gradient Size (Euclidian Norm) - A gradient threshold must be provided
-def run(learning_rate = False, num_iterations = False, messages = True, stopping_criteria = 1, gradient_threshold = 0):
+#   3 - Normal Equations
+def run(learning_rate = False, num_iterations = False, messages = True, gradient_threshold = 1, running_mode = 1):
     if(not messages):
         global verbose
         verbose = False
     
     global RSS_points, gradient_size, iterations
     RSS_points = []
-    gradient_size = []
+    gradient_norms = []
     iterations = []
     
     points = genfromtxt('income.csv', delimiter=',')
@@ -190,7 +130,14 @@ def run(learning_rate = False, num_iterations = False, messages = True, stopping
             .format(initial_b, initial_m, compute_error_for_given_points(initial_b, initial_m, points)))
         print("Running...")
 
-    b, m, gradient_norms, n_iterations = gradient_desc_runner(points, initial_b, initial_m, learning_rate, num_iterations, stopping_criteria, gradient_threshold)
+    results = gradient_desc_runner(points, initial_b, initial_m, learning_rate, num_iterations, running_mode, gradient_threshold)
+
+    b = results[0]
+    m = results[1]
+    if(len(results) > 2):
+        gradient_norms = results[2]
+    if(len(results) > 3):
+        num_iterations = results[3]
 
     if(verbose):
         print("After {0} iterations b = {1}, m = {2}, error = {3}"
@@ -201,5 +148,5 @@ def run(learning_rate = False, num_iterations = False, messages = True, stopping
         "m": m,
         "RSS": RSS_points,
         "gradient_norms": gradient_norms,
-        "gradient_size": gradient_size,
-        "n_iterations": n_iterations}
+        "n_iterations": num_iterations
+        }
